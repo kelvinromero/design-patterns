dec9ba221f1617cff74698461b3f99a87a4ae659
Moreira-Junior <junior_moreira88@hotmail.com>
2023-06-29 21:33:33 -0300

    fix main

--- SSP/src/main/java/org/example/Main.java
+++ SSP/src/main/java/org/example/Main.java
@@ -13,18 +13,23 @@ public class Main {
     public static void main(String[] args) {
         Curriculum curriculum = new Curriculum("John Doe", "123456789", Title.DOCTORATE, 70);
         curriculum.addExperience(new Experience(ExperienceType.TEACHING_FEDERAL_INSTITUTE, "Teaching at University of London", 10));
-        curriculum.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_COORDINATOR, "Director", 5));
+        curriculum.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_COORDINATOR, "Coordinator", 5));
 
         Curriculum curriculum2 = new Curriculum("Maria Doe", "223456789", Title.MASTER, 80);
         curriculum2.addExperience(new Experience(ExperienceType.TEACHING_FEDERAL_INSTITUTE, "Teaching at University of London", 10));
-        curriculum2.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_COORDINATOR, "Director", 5));
+        curriculum2.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_COORDINATOR, "Coordinator", 5));
 
         Curriculum curriculum3 = new Curriculum("Pedro Doe", "323456789", Title.MASTER, 80);
         curriculum3.addExperience(new Experience(ExperienceType.TEACHING_FEDERAL_INSTITUTE, "Teaching at University of London", 10));
-        curriculum3.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_COORDINATOR, "Director", 5));
-        curriculum3.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_DIRECTOR, "Director", 10));
+        curriculum3.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_COORDINATOR, "Coordinator", 5));
+        curriculum3.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_DIRECTOR, "Director", 16));
 
-        List<CurriculumInterface> curriculums = Arrays.asList(curriculum, curriculum2, curriculum3);
+        Curriculum curriculum4 = new Curriculum("Julyana Doe", "323456789", Title.SPECIALIST, 50);
+        curriculum4.addExperience(new Experience(ExperienceType.TEACHING_FEDERAL_INSTITUTE, "Teaching at University of London", 10));
+        curriculum4.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_COORDINATOR, "Director", 5));
+        curriculum4.addExperience(new Experience(ExperienceType.ACADEMIC_MANAGEMENT_DIRECTOR, "Director", 10));
+
+        List<CurriculumInterface> curriculums = Arrays.asList(curriculum, curriculum2, curriculum3, curriculum4);
 
         DidaticsEvaluator didaticsEvaluator = new DidaticsEvaluator();
         TitleEvaluator titleEvaluator = new TitleEvaluator();
@@ -37,6 +42,8 @@ public class Main {
 
         curriculums = ssp.getPssResult(curriculums);
 
-        System.out.println(curriculums);
+        for(CurriculumInterface cv : curriculums) {
+            System.out.println(cv);
+        }
     }
 }
